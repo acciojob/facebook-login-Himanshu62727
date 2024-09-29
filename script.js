6b337b188efe475f5a34ae33213b05ac2a74d3c8
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-09-29 13:58:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+let emailname = document.getElementById('email');
+let password = document.getElementById('password');
+let flag = 
+
+function validateFrom() {
+	if (emailname.value == "") {
+		document.getElementById('emailname').innerHTML = "Please Correct Email address"
+	}
+	else if (emailname.value.length < 12) {
+		document.getElementById('emailname').innerHTML = "Only 12 more than email id"
+	}
+
+	else {
+		document.getElementById('emailname').innerHTML= ""
+	}
+
+	if (password.value == "") {
+		document.getElementById('password'.innerHTML = "Password is not correct")
+	}
+	return false;
+}
